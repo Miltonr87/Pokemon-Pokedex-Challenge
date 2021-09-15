bddc0a0d21d477dc960e84ac0287c94befc0556a
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-09-14 21:11:51 -0300

    pokemon limit reduced to 888

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = "https://pokeapi.co/api/v2/";
-const pokemonLimit = "pokemon?limit=897";
+const pokemonLimit = "pokemon?limit=888";
 
 export const fetchApi = axios.get(`${api}${pokemonLimit}`);
